b4a030946f5d906377156887be65e07999930fb4
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2025-08-26 22:04:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Welcome to the application!");
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+  const completion = await groq.chat.completions.create({
+    temperature: 1,
+    top_p: 1,
+    model: "llama-3.3-70b-versatile",
+    messages: [
+       {
+        role: "system",
+        content: 'You are Jarvis, a smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. Output must be a single word.',
+      
+      },
+      { 
+        role: "user", 
+        content: 'Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week '
+      }, 
+    
+      
+    ],
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
