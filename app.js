5523fbab3c83360099913b8ea218aff2f0a15ccb
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2025-09-04 23:00:09 +0530

    dynamic tool calling

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ const tvly = tavily({
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
-  const completion = await groq.chat.completions.create({
-    model: "llama-3.3-70b-versatile",
-    temperature: 0,
-    messages: [
-      {
+
+  const messages = [
+    {
         role: "system",
         content: `You are a smart assistant who answers the asked questions.
         1. searchWeb({query}: {query:string}) // Search the latest information and realtime data on the internet`
@@ -21,7 +19,67 @@ async function main() {
         role: "user",
         content: "When was iPhone 16 launched?"
       }
+    ];
+
+
+    while (true) {
+      const completion = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+    messages: messages,
+    tools: [
+     {
+      "type": "function",
+      "function": {
+        "name": "webSearch",
+        "description": "Search the latest information and realtime data on the internet",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "query": {
+              "type": "string",
+              "description": "The search query to perform search on."
+            },
+          
+          },
+          "required": ["query"]
+        }
+      }
+    }
     ],
+    tool_choice: "auto"
+  });
+  
+  messages.push(completion.choices[0].message);
+  
+const toolCalls = completion.choices[0].message.tool_calls
+if(!toolCalls){
+  console.log(`Final Answer: ${completion.choices[0].message.content}`);
+  return;
+}
+
+for (const tool of toolCalls) {
+  console.log('toolL:',tool)
+  const functionName = tool.function.name;
+  const functionParams = tool.function.arguments;
+
+  if (functionName === "webSearch") {
+   const toolResult = await webSearch(JSON.parse(functionParams));
+    console.log('toolResult:',toolResult);
+
+    messages.push({
+      tool_call_id: tool.id,
+      role: "tool",
+      name: functionName,
+      content: toolResult
+    });
+  }
+}
+
+  const completion2 = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+    messages: messages,
     tools: [
      {
       "type": "function",
@@ -45,25 +103,10 @@ async function main() {
     tool_choice: "auto"
   });
 
+  console.log(JSON.stringify(completion2.choices[0].message, null, 2));
+    }
+
   
-const toolCalls = completion.choices[0].message.tool_calls
-if(!toolCalls){
-  console.log(`Final Answer: ${completion.choices[0].message.content}`);
-  return;
-}
-
-for (const tool of toolCalls) {
-  console.log('toolL:',tool)
-  const functionName = tool.function.name;
-  const functionParams = tool.function.arguments;
-
-  if (functionName === "webSearch") {
-   const toolResult = await webSearch(JSON.parse(functionParams));
-    console.log('toolResult:',toolResult);
-  }
-}
-
-  console.log(JSON.stringify(completion.choices[0].message, null, 2));
 }
 
 main();
@@ -76,5 +119,9 @@ async function webSearch({query}) {
 
   const response = await tvly.search(query);
   console.log('response:',response);
-   return 'Iphone 16 was launched on 20 september 2024';
+
+  const finalResult = response.results.map(result => result.content).join('\n\n');
+  console.log('finalResult:',finalResult);
+  
+   return finalResult;
    }
