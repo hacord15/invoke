871d3a98bf10e9635fc34b554c2cbd51f84696f7
Abhineet Pal <111232688+hacord15@users.noreply.github.com>
2025-09-05 00:02:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import readline from "readline/promises";
 import Groq from "groq-sdk";
 import { tavily } from "@tavily/core";
 
@@ -9,20 +10,38 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
   const messages = [
     {
         role: "system",
         content: `You are a smart assistant who answers the asked questions.
-        1. searchWeb({query}: {query:string}) // Search the latest information and realtime data on the internet`
+        1. searchWeb({query}: {query:string}) // Search the latest information and realtime data on the internet.
+        current date: ${new Date().toUTCString()}`
+        // `
       },
-      {
-        role: "user",
-        content: "When was iPhone 16 launched?"
-      }
+      // {
+      //   role: "user",
+      //   content: "Weather of mumbai today?"
+      // }
     ];
 
 
-    while (true) {
+ while (true) { 
+    
+    const question = await rl.question("You:");
+
+    //bye  
+
+    if (question === 'bye') {
+      break;
+    }
+    messages.push({ role: "user", content: question });
+
+     while (true) {
       const completion = await groq.chat.completions.create({
     model: "llama-3.3-70b-versatile",
     temperature: 0,
@@ -55,17 +74,17 @@ async function main() {
 const toolCalls = completion.choices[0].message.tool_calls
 if(!toolCalls){
   console.log(`Final Answer: ${completion.choices[0].message.content}`);
-  return;
+  break;
 }
 
 for (const tool of toolCalls) {
-  console.log('toolL:',tool)
+  //console.log('toolL:',tool)
   const functionName = tool.function.name;
   const functionParams = tool.function.arguments;
 
   if (functionName === "webSearch") {
    const toolResult = await webSearch(JSON.parse(functionParams));
-    console.log('toolResult:',toolResult);
+ //   console.log('toolResult:',toolResult);
 
     messages.push({
       tool_call_id: tool.id,
@@ -105,6 +124,7 @@ for (const tool of toolCalls) {
 
   console.log(JSON.stringify(completion2.choices[0].message, null, 2));
     }
+ }
 
   
 }
@@ -118,10 +138,10 @@ async function webSearch({query}) {
   console.log("Calling the web");
 
   const response = await tvly.search(query);
-  console.log('response:',response);
+  //console.log('response:',response);
 
   const finalResult = response.results.map(result => result.content).join('\n\n');
-  console.log('finalResult:',finalResult);
+  //console.log('finalResult:',finalResult);
   
    return finalResult;
    }
